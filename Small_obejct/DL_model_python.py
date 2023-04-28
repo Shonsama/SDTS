6bc4d7961bad6dc63a205a9a153ee5b1ffef374d
William yan <1337842463@qq.com>
2023-04-28 20:20:59 +0800

    The database in small object processing right now

---
+++ Small_obejct/DL_model_python.py
@@ -0,0 +1,116 @@
+import os
+import numpy as np
+import keras
+from keras.layers import Dense,Dropout
+from scipy.io import loadmat, savemat
+
+# Model training function
+def train(In_train, Out_train, In_test, Out_test,
+          epochs, batch_size,dr,
+          num_hidden_layers, nodes_per_layer,
+          loss_fn,n_BS,n_beams):
+    
+    in_shp = list(In_train.shape[1:])
+
+    AP_models = []
+    
+    for bs_idx in range(0, n_BS):
+        idx = bs_idx*n_beams
+        idx_str = 'BS%i'%bs_idx
+        model = keras.Sequential()
+        model.add(Dense(nodes_per_layer, activation='relu', kernel_initializer='he_normal', input_shape=in_shp))
+        model.add(Dropout(dr))
+        for h in range(num_hidden_layers):
+            model.add(Dense(nodes_per_layer, activation='relu', kernel_initializer='he_normal'))
+            model.add(Dropout(dr))
+        
+        model.add(Dense(n_beams, activation='relu', kernel_initializer='he_normal'))
+        model.compile(loss=loss_fn, optimizer='adam')
+        model.summary()
+        
+        filepath = 'DLCB_code_output/Results_mmWave_ML_BS' + idx_str
+        
+        if not os.path.exists(filepath):
+            os.makedirs(filepath)
+
+        history = model.fit(In_train,
+                            Out_train[:, idx:idx + n_beams],
+                            batch_size=batch_size,
+                            epochs=epochs,
+                            verbose=2,
+                            validation_data=(In_test, Out_test[:,idx:idx + n_beams]),
+                            callbacks = [
+                                keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, mode='auto'),
+                                keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+                            ])
+    
+        model.load_weights(filepath)
+        
+        AP_models.append(model)
+        
+        
+    return AP_models
+
+# Reading input and output sets generated from MATLAB
+# import hdf5storage
+# In_set_file = hdf5storage.loadmat('DL_input.mat')
+# Out_set_file = hdf5storage.loadmat('DL_output.mat')
+
+
+# # In_set=In_set_file['DL_input']
+# # Out_set=Out_set_file['DL_output']
+# In_set = np.array([1,2],[1,2])
+# Out_set = np.array([1,2],[1,2])
+# # Parameter initialization
+# num_user_tot=In_set.shape[0]
+# n_DL_size=[.001,.05,.1,.15,.2,.25,.3,.35,.4,.45,.5,.55,.6,.65,.7]
+# count=0
+# num_tot_TX=4
+# num_beams=512
+#
+# for DL_size_ratio in n_DL_size:
+#
+#     print (DL_size_ratio)
+#     count=count+1
+#     DL_size=int(num_user_tot*DL_size_ratio)
+#
+#     np.random.seed(2016)
+#     n_examples = DL_size
+#     num_train  = int(DL_size * 0.8)
+#     num_test   = int(num_user_tot*.2)
+#
+#     train_index = np.random.choice(range(0,num_user_tot), size=num_train, replace=False)
+#     rem_index = set(range(0,num_user_tot))-set(train_index)
+#     test_index= list(set(np.random.choice(list(rem_index), size=num_test, replace=False)))
+#
+#     In_train = In_set[train_index]
+#     In_test =  In_set[test_index]
+#
+#     Out_train = Out_set[train_index]
+#     Out_test = Out_set[test_index]
+#
+#
+#     # Learning model parameters
+#     epochs = 10
+#     batch_size = 100
+#     dr = 0.05                  # dropout rate
+#     num_hidden_layers=4
+#     nodes_per_layer=In_train.shape[1]
+#     loss_fn='mean_squared_error'
+#
+#     # Model training
+#     AP_models = train(In_train, Out_train, In_test, Out_test,
+#                                           epochs, batch_size,dr,
+#                                           num_hidden_layers, nodes_per_layer,
+#                                           loss_fn,num_tot_TX,num_beams)
+#
+#     # Model running/testing
+#     DL_Result={}
+#     for idx in range(0,num_tot_TX,1):
+#         beams_predicted=AP_models[idx].predict( In_test, batch_size=10, verbose=0)
+#
+#         DL_Result['TX'+str(idx+1)+'Pred_Beams']=beams_predicted
+#         DL_Result['TX'+str(idx+1)+'Opt_Beams']=Out_test[:,idx*num_beams:(idx+1)*num_beams]
+#
+#     DL_Result['user_index']=test_index
+#     savemat('DLCB_code_output/DL_Result' + str(count) + '.mat',DL_Result)
---
+++ Small_obejct/README.md
@@ -0,0 +1,14 @@
+# Suspect_Detection
+You need to download the weights file if you need to run the high resolution generation module, ane set your file folder like this.
+
+./model/vgg19/.npy
+
+./models/d.npz  
+./models/g.npz  
+Link to this:https://drive.google.com/drive/folders/1gMXBlnnvMNmkwV4a95Wp1FfkuW0Mp7ZS?usp=share_link
+
+replace the img file path in ./train.py evaluate_2
+run the train.py
+```
+python train.py
+```
